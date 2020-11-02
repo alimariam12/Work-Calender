0bc235a40bd6ffb71df09eda0a7bff25920c7365
Mariam Ali <alim12@csp.edu>
2020-11-02 14:25:56 -0600

    Final

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 
    
 //save hours to local storage
-$("#hour9").val(localStorage.getItem("hour-9"));
-$("#hour10").val(localStorage.getItem("hour-10"));
-$("#hour11").val(localStorage.getItem("hour-11"));
-$("#hour12").val(localStorage.getItem("hour-12"));
-$("#hour13").val(localStorage.getItem("hour-13"));
-$("#hour14").val(localStorage.getItem("hour-14"));
-$("#hour15").val(localStorage.getItem("hour-15"));
-$("#hour16").val(localStorage.getItem("hour-16"));
-$("#hour17").val(localStorage.getItem("hour-17"));
+$("#hour9").val(localStorage.getItem("hour.9"));
+$("#hour10").val(localStorage.getItem("hour.10"));
+$("#hour11").val(localStorage.getItem("hour.11"));
+$("#hour12").val(localStorage.getItem("hour.12"));
+$("#hour13").val(localStorage.getItem("hour.13"));
+$("#hour14").val(localStorage.getItem("hour.14"));
+$("#hour15").val(localStorage.getItem("hour.15"));
+$("#hour16").val(localStorage.getItem("hour.16"));
+$("#hour17").val(localStorage.getItem("hour.17"));
 
 $(".saveBtn").on("click", function(){
 let timeBlock = $(this).parent().attr("id");
@@ -25,7 +25,7 @@ function trackTime (){
     $(".time-block").each(function () {
     
         console.log("hello")
-            let allTime = parseInt($(this).attr("id").split("-")[1]);
+            let allTime = parseInt($(this).attr("id").split(".")[1]);
         
             if (allTime < currentTime){
                 $(this).addClass("past");
