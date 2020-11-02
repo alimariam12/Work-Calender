335b3362624f185673c7df13c1fce98f87e6fcf7
Mariam Ali <alim12@csp.edu>
2020-11-01 22:29:28 -0600

    fixed

--- js.js
+++ js.js
@@ -1,19 +1,43 @@
 $(document).ready(function(){
     $(currentDay).text(moment().format('MMMM Do YYYY'));
    
-
 //save hours to local storage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-$("#hour2 .description").val(localStorage.getItem("hour2"));
-$("#hour3 .description").val(localStorage.getItem("hour3"));
-$("#hour4 .description").val(localStorage.getItem("hour4"));
-$("#hour5 .description").val(localStorage.getItem("hour5"));
+$("#hour9 .description").val(localStorage.getItem("hour-9"));
+$("#hour10 .description").val(localStorage.getItem("hour-10"));
+$("#hour11 .description").val(localStorage.getItem("hour-11"));
+$("#hour12 .description").val(localStorage.getItem("hour-12"));
+$("#hour13 .description").val(localStorage.getItem("hour-13"));
+$("#hour14 .description").val(localStorage.getItem("hour-14"));
+$("#hour15 .description").val(localStorage.getItem("hour-15"));
+$("#hour16 .description").val(localStorage.getItem("hour-16"));
+$("#hour17 .description").val(localStorage.getItem("hour-17"));
 
+
+
+function trackTime (){
+    let currentTime = moment().hours();
+    console.log(currentTime)
+    $(".time-block").each(function () {
+    
+        console.log("hello")
+            let allTime = parseInt($(this).attr("id").split("-")[1]);
+        
+            if (allTime < currentTime){
+                $(this).addClass("past");
+            }
+            console.log($(this))
+            if (allTime === currentTime){
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            }
+            if (allTime > currentTime){
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+            })
+        
+
+}
+trackTime()
 })
-let currentTime = moment().format("H:mm a")
-let allTime = parseInt($(this).attr("id"))
-
