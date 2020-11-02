8ce91a91c0ac58df3f8a165c60d1e6d89459ba98
Mariam Ali <alim12@csp.edu>
2020-11-01 19:49:37 -0600

    working

--- js.js
+++ js.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $(currentDay).text(moment().format('MMMM Do YYYY'));
-
    
+
+//save hours to local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
+
 })
+let currentTime = moment().format("H:mm a")
+let allTime = parseInt($(this).attr("id"))
 
